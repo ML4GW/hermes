e159643ecc9e21153485d15ae8bae87e530135a5
Alec Gunny <alec.gunny@gmail.com>
2022-08-22 18:35:24 -0700

    getting TRT working

--- hermes/hermes.quiver/hermes/quiver/exporters/tensorrt/onnx.py
+++ hermes/hermes.quiver/hermes/quiver/exporters/tensorrt/onnx.py
@@ -38,9 +38,6 @@ def _convert_network(
     # batch size, the config's value will read 0,
     # so replace with 1 as a default here for streaming
     builder.max_batch_size = max(model_config.max_batch_size, 1)
-    if use_fp16:
-        builder.fp16_mode = True
-        builder.strict_type_constraints = True
 
     # if any of the inputs have a variable
     # length batch dimension, create an
@@ -48,6 +45,10 @@ def _convert_network(
     # the most optimized batch size being the largest
     config = stack.enter_context(builder.create_builder_config())
     config.max_workspace_size = 1 << 28
+    if use_fp16:
+        config.flags |= 1 << int(trt.BuilderFlag.FP16)
+        # builder.strict_type_constraints = True
+
     for input in model_config.input:
         if input.dims[0] != -1:
             # this input doesn't have a variable
@@ -78,7 +79,17 @@ def _convert_network(
         )
     )
     parser = stack.enter_context(trt.OnnxParser(network, logger))
-    parser.parse(model_binary)
+    success = parser.parse(model_binary)
+    if not success:
+        errors = [parser.get_error(i) for i in range(parser.num_errors)]
+        errors = "\n".join(map(str, errors))
+        completed_layers = [
+            network.get_layer(i).name for i in range(network.num_layers)
+        ]
+        msg = "Parsing ONNX binary failed. Completed layers:\n"
+        msg += "\n".join(completed_layers)
+        msg += "\n\nRaised errors:\n" + errors
+        raise RuntimeError(msg)
 
     if len(model_config.output) == 1 and network.num_outputs == 0:
         # if we only have a single output and for whatever
@@ -112,11 +123,13 @@ def _convert_network(
         if len(network_output.shape) != len(output.dims):
             raise ValueError(
                 "Number of dimensions {} specified for "
-                "output {} not equal to number {} found "
-                "in TensorRT network".format(
+                "output {} with shape {} not equal to number {} found "
+                "in TensorRT network with shape {}".format(
                     len(output.dims),
                     output.name,
+                    output.dims,
                     len(network_output.shape),
+                    network_output.shape
                 )
             )
 
