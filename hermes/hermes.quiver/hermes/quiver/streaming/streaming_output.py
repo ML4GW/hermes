9f60675c91c1b737cdce1207b272a10b700b65db
Alec Gunny <alec.gunny@gmail.com>
2022-10-24 10:28:22 -0700

    making tensor-op based online averager

--- hermes/hermes.quiver/hermes/quiver/streaming/streaming_output.py
+++ hermes/hermes.quiver/hermes/quiver/streaming/streaming_output.py
@@ -9,6 +9,21 @@ if TYPE_CHECKING:
     from hermes.quiver.model import ExposedTensor
 
 
+def window(x: torch.Tensor, num_windows: int, stride: int):
+    if x.ndim == 2:
+        num_channels = len(x)
+        x = x.view(1, num_channels, 1, -1)
+    else:
+        x = x.view(1, 1, 1, -1)
+        num_channels = 1
+
+    x = torch.nn.functional.unfold(
+        x, kernel_size=(1, num_windows), dilation=(1, stride)
+    )
+    x = x.reshape(num_channels, num_windows, -1)
+    return x.transpose(1, 0)
+
+
 class OnlineAverager(torch.nn.Module):
     def __init__(
         self,
@@ -21,14 +36,27 @@ class OnlineAverager(torch.nn.Module):
         self.update_size = update_size
         self.num_updates = num_updates
         self.batch_size = batch_size
-        self.snapshot_size = update_size * num_updates
+        self.num_channels = num_channels
 
-        normalizer = torch.arange(num_updates) + 1
-        normalizer = normalizer.flip(-1)
-        normalizer = torch.repeat_interleave(normalizer, update_size)
+        snapshot_size = (batch_size + num_updates - 1) * update_size
+        if num_channels is None:
+            blank = torch.zeros((batch_size, snapshot_size))
+        else:
+            blank = torch.zeros((batch_size, num_channels, snapshot_size))
+        self.register_buffer("blank", blank)
 
-        self.register_buffer("normalizer", normalizer)
-        self.register_buffer("zero", torch.zeros((1,)))
+        idx = torch.arange(num_updates * update_size)
+        idx = torch.stack([idx + i * update_size for i in range(batch_size)])
+        if num_channels is not None:
+            idx = idx.view(batch_size, 1, -1).repeat(1, num_channels, 1)
+        self.register_buffer("idx", idx)
+
+        weights = torch.scatter(blank, -1, idx, 1).sum(0)
+        weights = window(weights, batch_size, update_size)
+        if num_channels is None:
+            weights = weights[:, 0]
+
+        self.register_buffer("weights", weights)
 
         pad_shape = (update_size * batch_size,)
         if num_channels is not None:
@@ -37,36 +65,33 @@ class OnlineAverager(torch.nn.Module):
         self.register_buffer("pad", pad)
 
     def forward(
-        self,
-        update: torch.Tensor,
-        snapshot: torch.Tensor,
-        update_idx: torch.Tensor,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        for i in range(self.batch_size):
-            if update.ndim > 2:
-                x = update[i, :, -self.snapshot_size :]
-            else:
-                x = update[i, -self.snapshot_size :]
+        self, update: torch.Tensor, snapshot: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        if self.num_channels is None:
+            update = update[:, None]
 
-            weights = self.normalizer.clamp(self.zero, update_idx + i + 1)
-            start = i * self.update_size
-            stop = start + x.shape[-1]
+        keep = self.num_updates * self.update_size
+        x = update[:, :, -keep:] / self.num_updates
 
-            if update.ndim > 2:
-                prev = snapshot[:, start:stop]
-                snapshot[:, start:stop] += (x - prev) / weights
-            else:
-                prev = snapshot[start:stop]
-                snapshot[start:stop] += (x - prev) / weights
+        windowed = window(snapshot, self.batch_size, self.update_size)
+        if self.num_channels is None:
+            windowed = windowed[:, 0]
+            x = x[:, 0]
+        windowed /= self.weights
+        windowed += x
 
-        output_size = self.update_size * self.batch_size
-        snapshot_size = snapshot.shape[-1] - output_size
-        output, snapshot = torch.split(
-            snapshot, [output_size, snapshot_size], dim=-1
-        )
+        padded = torch.scatter(self.blank, -1, self.idx, windowed)
+        snapshot = padded.sum(axis=0)
 
-        snapshot = torch.concat([snapshot, self.pad], axis=-1)
-        return output[None], snapshot, update_idx + self.batch_size
+        if self.num_updates == 1:
+            return snapshot[None], self.pad
+        else:
+            splits = [self.batch_size, self.num_updates - 1]
+            output, snapshot = torch.split(
+                snapshot, [i * self.update_size for i in splits], dim=-1
+            )
+        snapshot = torch.cat([snapshot, self.pad], axis=-1)
+        return output[None], snapshot
 
 
 def make_streaming_output_model(
@@ -127,8 +152,8 @@ def make_streaming_output_model(
         name=name or "aggregator",
         input_name="update",
         input_shape=(batch_size,) + input.shape[1:],
-        state_names=["online_average", "update_index"],
-        state_shapes=[snapshot_shape, (1,)],
+        state_names=["online_average"],
+        state_shapes=[snapshot_shape],
         output_names=["output_stream"],
         streams_per_gpu=streams_per_gpu,
     )
